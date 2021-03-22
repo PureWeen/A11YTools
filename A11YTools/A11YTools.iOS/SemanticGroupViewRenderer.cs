6cfc81a208c7eb833de3c96989331b40403deffd
Shane Neuville <shneuvil@microsoft.com>
2021-03-22 16:18:48 -0500

    - ios parts

---
+++ A11YTools/A11YTools.iOS/SemanticGroupViewRenderer.cs
@@ -0,0 +1,12 @@
+﻿using A11YTools;
+using A11YTools.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(SemanticView), typeof(SemanticGroupViewRenderer))]
+namespace A11YTools.Droid
+{
+    public class SemanticGroupViewRenderer : VisualElementRenderer<SemanticView>
+    {
+    }
+}
---
+++ A11YTools/A11YTools.iOS/SemanticViewRenderer.cs
@@ -0,0 +1,16 @@
+﻿using A11YTools;
+using A11YTools.Droid;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(SemanticView), typeof(SemanticViewRenderer))]
+namespace A11YTools.Droid
+{
+    public class SemanticViewRenderer : VisualElementRenderer<SemanticView>
+    {
+        public SemanticViewRenderer()
+        {
+        }
+    }
+}
